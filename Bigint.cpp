7fc3c31591b3422962c61a06f3528bb93af31334
Gajanan-Sapsod <105559761+Gajanan-Sapsod@users.noreply.github.com>
2023-08-24 16:35:53 +0530

    Update Bigint.cpp

--- Bigint.cpp
+++ Bigint.cpp
@@ -1,215 +1,225 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
+#define size 23
+typedef struct bigint_type
+{
+    int arr[size];
+    int length;
+}bigint;
 
-class BigInt {
-    string digits;
-
-public:
-    // Constructors to initialise digits
-    BigInt(unsigned long long n = 0);
-    BigInt(string &);
-    BigInt(const char *);
-    BigInt(BigInt &);
-
-    // Helper Functions:
-    friend bool Null(const BigInt &);
-    friend int Length(const BigInt &);
-    friend bool operator<(const BigInt &, const BigInt &);
-
-    // Direct assignment
-    BigInt &operator=(const BigInt &);
-
-    // Addition and Subtraction
-    friend BigInt &operator+=(BigInt &, const BigInt &);
-    friend BigInt operator+(const BigInt &, const BigInt &);
-    friend BigInt operator-(const BigInt &, const BigInt &);
-    friend BigInt &operator-=(BigInt &, const BigInt &);
-
-    // Multiplication
-    friend BigInt &operator*=(BigInt &, const BigInt &);
-    friend BigInt operator*(const BigInt &, const BigInt &);
-
-    // Read and Write
-    friend ostream &operator<<(ostream &, const BigInt &);
-    friend istream &operator>>(istream &, BigInt &);
-};
-
-BigInt::BigInt(string &s) {
-    digits = "";
-    int n = s.size();
-    for (int i = n - 1; i >= 0; i--) {
-        if (!isdigit(s[i]))
-            throw("ERROR");
-        digits.push_back(s[i] - '0');
+bigint input()
+{
+    char temp[size];
+    int length_t=0;
+    char t;
+    int i=0;
+    scanf("%c", &t);
+    while(t!='\n')
+    {
+       temp[i]=t;
+       length_t++;
+       i++;
+       scanf("%c", &t);
     }
-}
-
-BigInt::BigInt(unsigned long long nr) {
-    do {
-        digits.push_back(nr % 10);
-        nr /= 10;
-    } while (nr);
-}
-
-BigInt::BigInt(const char *s) {
-    digits = "";
-    for (int i = strlen(s) - 1; i >= 0; i--) {
-        if (!isdigit(s[i]))
-            throw("ERROR");
-        digits.push_back(s[i] - '0');
+    int itr=length_t-1;
+    i=0;
+    bigint b;
+    b.length=0;
+    while(itr>=0)
+    {
+       b.arr[i]=temp[itr]-'0';
+       b.length++;
+       itr--;
+       i++;
     }
+    return b;
+    
 }
 
-BigInt::BigInt(BigInt &a) {
-    digits = a.digits;
-}
-
-bool Null(const BigInt &a) {
-    if (a.digits.size() == 1 && a.digits[0] == 0)
-        return true;
-    return false;
-}
-
-int Length(const BigInt &a) {
-    return a.digits.size();
-}
-
-BigInt &BigInt::operator=(const BigInt &a) {
-    digits = a.digits;
-    return *this;
-}
-
-bool operator<(const BigInt &a, const BigInt &b) {
-    int n = Length(a), m = Length(b);
-    if (n != m)
-        return n < m;
-    while (n--)
-        if (a.digits[n] != b.digits[n])
-            return a.digits[n] < b.digits[n];
-    return false;
-}
-
-BigInt &operator+=(BigInt &a, const BigInt &b) {
-    int t = 0, s, i;
-    int n = Length(a), m = Length(b);
-    if (m > n)
-        a.digits.append(m - n, 0);
-    n = Length(a);
-    for (i = 0; i < n; i++) {
-        if (i < m)
-            s = (a.digits[i] + b.digits[i]) + t;
-        else
-            s = a.digits[i] + t;
-        t = s / 10;
-        a.digits[i] = (s % 10);
+void output(bigint b)
+{
+    b.length-=1;
+    while(b.length>=0)
+    {
+      printf("%d", b.arr[b.length]);
+      b.length-=1;
     }
-    if (t)
-        a.digits.push_back(t);
-    return a;
+    printf("\n");
 }
 
-BigInt operator+(const BigInt &a, const BigInt &b) {
-    BigInt temp;
-    temp = a;
-    temp += b;
-    return temp;
-}
-
-BigInt &operator-=(BigInt &a, const BigInt &b) {
-    if (a < b)
-        throw("UNDERFLOW");
-    int n = Length(a), m = Length(b);
-    int i, t = 0, s;
-    for (i = 0; i < n; i++) {
-        if (i < m)
-            s = a.digits[i] - b.digits[i] + t;
-        else
-            s = a.digits[i] + t;
-        if (s < 0)
-            s += 10, t = -1;
-        else
-            t = 0;
-        a.digits[i] = s;
+bigint addition(bigint b1, bigint b2)
+{
+    int f=0,s=0, carry=0, add=0;
+    bigint ans;
+    ans.length=0;
+    while(f<b1.length && s<b2.length)
+    {
+        add=b1.arr[f]+b2.arr[s]+carry;
+        ans.arr[ans.length]=add%10;
+        ans.length++;
+        f++;
+        s++;
+        carry=add/10;
     }
-    while (n > 1 && a.digits[n - 1] == 0)
-        a.digits.pop_back(), n--;
-    return a;
-}
+    while(f<b1.length)
+    {
+        add=b1.arr[f]+carry;
+        ans.arr[ans.length]=add%10;
+        ans.length++;
+        f++;
+        carry=add/10;
 
-BigInt operator-(const BigInt &a, const BigInt &b) {
-    BigInt temp;
-    temp = a;
-    temp -= b;
-    return temp;
-}
-
-BigInt &operator*=(BigInt &a, const BigInt &b) {
-    if (Null(a) || Null(b)) {
-        a = BigInt();
-        return a;
     }
-    int n = a.digits.size(), m = b.digits.size();
-    vector<int> v(n + m, 0);
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++) {
-            v[i + j] += (a.digits[i]) * (b.digits[j]);
+
+    while(s<b2.length)
+    {
+        add=b2.arr[s]+carry;
+        ans.arr[ans.length]=add%10;
+        ans.length++;
+        s++;
+        carry=add/10;
+
+    }
+    if(carry!=0)
+    {
+        ans.arr[ans.length]=carry;
+        ans.length++;
+    }
+    return ans;
+    
+}
+
+bigint subtraction(bigint b1, bigint b2)
+{
+    int underflow=0;
+    int f=0, s=0, carry=0;
+    bigint ans;
+    ans.length=0;
+    while(f<b1.length && s<b2.length)
+    {
+        if(b1.arr[f]>=(b2.arr[s]+carry))
+        {
+           ans.arr[ans.length]=b1.arr[f]-(b2.arr[s]+carry);
+           carry=0;
         }
-    n += m;
-    a.digits.resize(v.size());
-    for (int s, i = 0, t = 0; i < n; i++) {
-        s = t + v[i];
-        v[i] = s % 10;
-        t = s / 10;
-        a.digits[i] = v[i];
+        else
+        {
+            ans.arr[ans.length]=(b1.arr[f]+10)-(b2.arr[s]+carry);
+            carry=1;
+        }
+        f++;
+        s++;
+        ans.length++;
     }
-    for (int i = n - 1; i >= 1 && !v[i]; i--)
-        a.digits.pop_back();
-    return a;
-}
+    while(f<b1.length)
+    {
 
-BigInt operator*(const BigInt &a, const BigInt &b) {
-    BigInt temp;
-    temp = a;
-    temp *= b;
-    return temp;
-}
-
-istream &operator>>(istream &in, BigInt &a) {
-    string s;
-    in >> s;
-    int n = s.size();
-    for (int i = n - 1; i >= 0; i--) {
-        if (!isdigit(s[i]))
-            throw("INVALID NUMBER");
-        a.digits[n - i - 1] = s[i];
+        if(b1.arr[f]>=(carry))
+        {
+           ans.arr[ans.length]=b1.arr[f]-(carry);
+           carry=0;
+        }
+        else
+        {
+            ans.arr[ans.length]=(b1.arr[f]+10)-(carry);
+            carry=1;
+        }
+        f++;
+        ans.length++;
+        
     }
-    return in;
+    if(s<b2.length)
+    {
+        underflow=1;
+    }
+    if(carry!=0)
+    {
+        underflow=1;
+    }
+    if(underflow==1)
+    {
+        ans.arr[0]=-1;
+        
+    }
+    return ans;
 }
 
-ostream &operator<<(ostream &out, const BigInt &a) {
-    for (int i = a.digits.size() - 1; i >= 0; i--)
-        out << (short)a.digits[i];
-    return out;
+bigint Multiply(bigint b1, bigint b2)
+{
+    int l1=b1.length;
+    int l2=b2.length;
+    bigint ans;
+    ans.length=0;
+    int  itr2=0;
+    while(itr2<l2)
+    {
+        bigint temp;
+        temp.length=0;
+        for(int i=0;i<itr2;i++)
+        {
+            temp.arr[temp.length]=0;
+            temp.length++;
+        }
+        int carry=0;
+        for(int i=0;i<l1;i++)
+        {
+            int prod=(b2.arr[itr2]*b1.arr[i])+carry;
+            carry=prod/10;
+            temp.arr[temp.length]=prod%10;
+            temp.length++;
+        }
+        if(carry!=0)
+        {
+            temp.arr[temp.length]=carry;
+            temp.length++;
+        }
+        
+        ans=addition(ans,temp);
+        itr2++;
+        
+
+    }
+
+    return ans;
 }
 
-// Driver code with some examples
-int main() {
-    BigInt first("12345");
-    cout << "The number of digits"
-         << " in first big integer = "
-         << Length(first) << '\n';
-    BigInt second(12345);
+int main()
+{
+    //1.input
+    bigint b1, b2;
+    printf("Enter two numbers : \n");
+    b1=input();
+    b2=input();
 
-    // Printing the numbers
-    cout << "first = " << first << '\n';
-    cout << "second = " << second << '\n';
+    int opt;
+    printf("Enter the 1, 2, 3 for additon, subtraction and multiplication and other number to exit\n");
+    scanf("%d", &opt);
+    
+    
+    switch(opt)
+    {
+        case 1:
+            bigint ans=addition(b1, b2);
+            output(ans);
+            break;
+        
+        case 2:
+            bigint sub=subtraction(b1, b2);
+            if(sub.arr[0]!=-1)
+            {
+                output(sub);
+            }
+            else
+            {
+                printf("There is a undeflow\n");
+            }
+            break;
+        
+        case 3:
+            bigint mul=Multiply(b1, b2);
+            output(mul);
+            break;
 
-    BigInt sum;
-    sum = (first + second);
-    cout << "Sum of fourth and fifth = "
-         << sum << '\n';
-    BigInt product;
-    product = second * first;
-    cout << "Product of second and third = "
-         << product << '\n';
+            
+    }
+    
 }
